5865834260055f57d3dd36632c9cc4424ab245ec
codeoftheghost <0x7ccghost@gmail.com>
2024-11-07 19:34:58 +0800

    adding sqlmap tool modified for inari linux and sqlmap.spec

---
+++ package/package-source/sqlmap/extra/shutils/pylint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# Copyright (c) 2006-2024 sqlmap developers (https://sqlmap.org/)
+# See the file 'LICENSE' for copying permission
+
+find . -wholename "./thirdparty" -prune -o -type f -iname "*.py" -exec pylint --rcfile=./.pylintrc '{}' \;
---
+++ package/package-source/sqlmap/lib/core/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""
+Copyright (c) 2006-2024 sqlmap developers (https://sqlmap.org/)
+See the file 'LICENSE' for copying permission
+"""
+
+pass
